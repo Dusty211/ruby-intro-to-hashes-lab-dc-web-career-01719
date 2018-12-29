b1dc680857697db3eef025d75294c5abe5bba586
Kyle Houghton <kylebenz@hotmail.com>
2018-12-29 01:37:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,18 +7,18 @@ def actor
 end
 
 def monopoly
-	monopoly = {
+  monopoly = {
+	  :railroads => {}
+	}
+end
+
+def monopoly_with_second_tier
+  monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
 	    :rent_in_dollars => {}
 	  }
-	}
-end
-
-def monopoly_with_second_tier
-	monopoly = {
-	  :railroads => {}
 	}
 end
 
